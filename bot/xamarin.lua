86fe24798917cab147583a6eea8af777166a4f63
mahdikord <saeed.199429@gmail.com>
2016-02-08 20:02:28 +0000

    Update xamarin.lua

--- bot/xamarin.lua
+++ bot/xamarin.lua
@@ -251,7 +251,7 @@ function create_config( )
     "plugins",
     "all",
     },
-    sudo_users = {119626024,0,tonumber(our_id)},--Sudo users
+    sudo_users = {165562299},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[⚡️ Xamarin Anti Spam Bot v1.7
